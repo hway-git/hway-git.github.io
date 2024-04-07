2eb3496e5d678854df89331c0ae8370e94e9692d
Madderbug <2670132411@qq.com>
2024-04-07 15:52:01 +0800

    -

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,10 +5,10 @@ export default {
     themeConfig: {
         nav: [ // 页面右上角的导航
             {
-                // text: '个人规划',
-                // items: [ // 可以配置成下拉
+                text: '个人规划',
+                items: [ // 可以配置成下拉
                 // { text: '2024个人规划', link: '/plan/2024的个人计划' },
-                // ]
+                ]
             }
         ],
         socialLinks: [{ icon: "github", link: "https://github.com/madderbughaha" }], // 可以连接到 github

9ccd87a17466f902ad043f2aea6c423aed5685bb
Madderbug <2670132411@qq.com>
2024-12-06 10:59:07 +0800

    *

--- app/app.config.ts
+++ app/app.config.ts
@@ -63,7 +63,6 @@ export default defineAppConfig({
             items: [
                 { icon: 'ph:files-bold', text: '文章', url: '/' },
                 { icon: 'ph:archive-bold', text: '归档', url: '/archive' },
-                { icon: 'ph:columns-bold', text: '专栏', url: '/column' },
             ],
         },
     ],
